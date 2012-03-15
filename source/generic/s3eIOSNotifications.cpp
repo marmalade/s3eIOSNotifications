a292790e29f633b99cf94e3d0de1ed111d3c3bbe
ElyAdam <adam.ely@marmalademail.com>
2012-03-15 17:20:14 +0000

    Update source/generic/s3eIOSNotifications.cpp

--- source/generic/s3eIOSNotifications.cpp
+++ source/generic/s3eIOSNotifications.cpp
@@ -1,16 +1,25 @@
 /*
-Generic implementation of the s3eIOSNotifications extension.
-This file should perform any platform-indepedentent functionality
-(e.g. error checking) before calling platform-dependent implementations.
-*/
-
+ * Copyright (C) 2001-2011 Ideaworks3D Ltd.
+ * All Rights Reserved.
+ *
+ * This document is protected by copyright, and contains information
+ * proprietary to Ideaworks Labs.
+ * This file consists of source code released by Ideaworks Labs under
+ * the terms of the accompanying End User License Agreement (EULA).
+ * Please do not use this program/source code before you have read the
+ * EULA and have agreed to be bound by its terms.
+ */
 /*
+ * Generic implementation of the s3eIOSNotifications extension.
+ * This file should perform any platform-indepedentent functionality
+ * (e.g. error checking) before calling platform-dependent implementations.
+ *
  * NOTE: This file was originally written by the extension builder, but will not
  * be overwritten (unless --force is specified) and is intended to be modified.
  */
 
-
 #include "s3eIOSNotifications_internal.h"
+
 s3eResult s3eIOSNotificationsInit()
 {
     //Add any generic initialisation code here
@@ -23,9 +32,9 @@ void s3eIOSNotificationsTerminate()
     s3eIOSNotificationsTerminate_platform();
 }
 
-s3eIOSNotificationsCallback s3eIOSNotificationsStartedWithNotification()
+s3eIOSNotificationsLaunchNotification s3eIOSNotificationsGetLaunchNotification()
 {
-    return s3eIOSNotificationsStartedWithNotification_platform();
+    return s3eIOSNotificationsGetLaunchNotification_platform();
 }
 
 const char* s3eIOSNotificationsGetRemoteNotificationToken()
